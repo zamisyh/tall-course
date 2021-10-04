b06bfc2eead3a3a9bb8b0f8f8e8d296d85364449
zam-dev <zamsyh.dev@gmail.com>
2021-10-04 14:45:56 +0700

    Layouting page admin auhor and users

---
+++ resources/views/livewire/dashboard/admin-author/home.blade.php
@@ -0,0 +1,12 @@
+<div>
+
+    <div class="drawer-content" x-data="{ drawer: false }">
+
+        @livewire('dashboard.components.navbar')
+        @include('livewire.dashboard.components.drawer')
+
+
+        Hallo World
+
+    </div>
+</div>
---
+++ resources/views/livewire/dashboard/users/home.blade.php
@@ -0,0 +1,14 @@
+<div>
+
+    <div class="drawer-content" x-data="{ drawer: false }">
+
+        @livewire('dashboard.components.navbar')
+        @include('livewire.dashboard.components.drawer')
+
+
+        Hallo World
+
+    </div>
+
+
+</div>
