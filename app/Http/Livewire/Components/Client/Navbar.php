280563e60b01b9f8304fdfec72fc166cc83f80ee
zam-dev <zamsyh.dev@gmail.com>
2021-10-04 14:42:19 +0700

    Add logout for all user in navbar

--- app/Http/Livewire/Components/Client/Navbar.php
+++ app/Http/Livewire/Components/Client/Navbar.php
@@ -3,11 +3,28 @@
 namespace App\Http\Livewire\Components\Client;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Navbar extends Component
 {
     public function render()
     {
         return view('livewire.components.client.navbar');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        $this->alert('success', 'Succesfully logout', [
+            'position' =>  'top-end',
+            'timer' =>  3000,
+            'toast' =>  true,
+            'text' =>  '',
+            'confirmButtonText' =>  'Ok',
+            'cancelButtonText' =>  'Cancel',
+            'showCancelButton' =>  false,
+            'showConfirmButton' =>  false,
+    ]);
     }
 }
