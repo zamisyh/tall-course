ca331ecb388aeffd1f99bd48b68330a1b6d719da
zam-dev <zamsyh.dev@gmail.com>
2021-10-04 14:47:27 +0700

    Create default role in model users

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $role = [
+           [
+               'name' => 'admin',
+               'guard_name' => 'web'
+           ],
+           [
+               'name' => 'author',
+               'guard_name' => 'web'
+           ],
+           [
+               'name' => 'user',
+               'guard_name' => 'web'
+           ]
+        ];
+
+        Role::insert($role);
+
+        $user = User::create([
+            'name' => 'super-admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('secrete')
+        ]);
+
+        $user->assignRole('admin');
+    }
+}
