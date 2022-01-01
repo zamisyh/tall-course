bbac85e91bdd088b2345fcb17facd7474504ee66
zam-dev <zamsyh.dev@gmail.com>
2022-01-01 10:37:33 +0700

    Little change

--- resources/views/livewire/dashboard/admin-author/author/course.blade.php
+++ resources/views/livewire/dashboard/admin-author/author/course.blade.php
@@ -67,7 +67,7 @@
                             <div class="form-control">
                                 <label class="mb-1 text-md">Description</label>
                                 <textarea rows="5" cols="5" wire:model='description' class="w-full textarea textarea-bordered @error('description')
-                                        input-error
+                                        textarea-error
                                 @enderror" placeholder="Input your name"></textarea>
                                 @error('description')
                                     <span class="text-red-700">{{ $message }}</span>
@@ -98,9 +98,9 @@
                           <thead>
                             <tr>
                               <th>No</th>
+                              <th>Image</th>
                               <th>Title</th>
                               <th>Description</th>
-                              <th>Image</th>
                               <th>Created At</th>
                               <th>Actions</th>
                             </tr>
@@ -109,11 +109,27 @@
                             @forelse ($data['course'] as $key => $item)
                                 <tr>
                                     <td>{{ $data['course']->firstItem() + $key }}</td>
+                                    <td><img src="{{ asset('storage/images/course/thumbnail/' . $item->image) }}" alt="image"></td>
                                     <td>{{ $item->title }}</td>
-                                    <td>{{ $item->description }}</td>
-                                    <td>{{ asset('storage/images/course/thumbnail/' . $item->image) }}</td>
+                                    <td>{{Str::substr( $item->description, 0, 55) }} ...</td>
                                     <td>{{ $item->created_at }}</td>
-                                    <td></td>
+                                    <td class="flex">
+                                        <span class="btn btn-primary btn-sm">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                                            </svg>
+                                        </span>
+                                        <span class="ml-1 btn btn-info btn-sm">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                              </svg>
+                                        </span>
+                                        <span wire:click='removeSeries({{ $item->id }})' class="ml-1 btn btn-error btn-sm">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
+                                        </span>
+                                    </td>
                                 </tr>
 
                                 @empty
@@ -122,6 +138,10 @@
                           </tbody>
                         </table>
                       </div>
+                </div>
+
+                <div class="mt-5">
+                    {{ $data['course']->links() }}
                 </div>
                 <div class="justify-center mt-5">
 
