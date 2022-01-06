bbf59e965b36ed1b661928206f59ceaad0b9ff4a
zam-dev <zamsyh.work@gmail.com>
2022-01-06 19:32:22 +0700

    Create controller episode

---
+++ app/Http/Livewire/Dashboard/AdminAuthor/Author/Components/AddEpisode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard\AdminAuthor\Author\Components;
+
+use Livewire\Component;
+
+class AddEpisode extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.admin-author.author.components.add-episode');
+    }
+}
