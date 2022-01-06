efe96bf4133348b5a3342b9719803dcfbbbbf069
zam-dev <zamsyh.work@gmail.com>
2022-01-06 19:31:29 +0700

    Create table section

---
+++ database/migrations/2022_01_06_031725_create_sections_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('series_id');
+            $table->unsignedBigInteger('author_id');
+            $table->text('name');
+            $table->timestamps();
+
+            $table->foreign('series_id')->references('id')->on('series')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
