34dc8dfea8272d4ac60aa6a28b594004d019adfa
zam-dev <zamsyh.work@gmail.com>
2022-01-07 19:07:16 +0700

    Create mounting for get data series

--- app/Http/Livewire/Client/LatestCourse.php
+++ app/Http/Livewire/Client/LatestCourse.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Livewire\Client;
 
+use App\Models\Section;
 use Livewire\Component;
+use App\Models\Series;
 
 class LatestCourse extends Component
 {
+
+    public $data_series, $sectionCount;
+
+    public function mount()
+    {
+        $this->data_series = Series::latest()->with('author', 'episode')->get();
+    }
+
     public function render()
     {
         return view('livewire.client.latest-course');
