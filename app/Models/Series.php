6c0951985a26588eb26529d9f8da159171877c55
zam-dev <zamsyh.work@gmail.com>
2022-01-07 19:10:03 +0700

    Add relation in model series

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Series extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function episode()
+    {
+        return $this->hasMany(Episode::class);
+    }
+
+    //
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
