fe72110c6adbc5182003a3e5ceb779bc33927f0d
zam-dev <zamsyh.work@gmail.com>
2022-01-07 19:11:22 +0700

    Showing data in latest course home

--- resources/views/livewire/client/latest-course.blade.php
+++ resources/views/livewire/client/latest-course.blade.php
@@ -1,32 +1,38 @@
 <div>
-    <div class="h-screen mt-5">
-        <div class="flex flex-wrap items-start mx-10 mt-5 transition duration-75 ease-in-out justify-items-start justify-betweeb">
-            <div class="m-2 my-2 overflow-hidden rounded shadow-md cursor-pointer md:max-w-sm sm:max-w-lg">
-                <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
-                <div class="px-6 py-4">
-                  <div class="mb-2 text-xl font-bold">TALL Stack For Future</div>
-                    <a href="" class="py-1 mr-2 text-blue-500">laravel</a>
-                    <a href="" class="py-1 mr-2 text-blue-500 ">livewire</a>
-                    <a href="" class="py-1 mr-2 text-blue-500">tailwind</a>
-                    <a href="" class="py-1 mr-2 text-blue-500">alpinejs</a>
-                  <p class="mt-5 text-base text-grey-darker">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                  </p>
-                </div>
-                <div class="flex justify-between px-6 py-4 text-xs text-gray-500">
-                    <div>
-                        <span class="font-bold">Andre Kub</span>
+
+        <div class="h-screen">
+            <div style="margin-top: -10%" class="flex flex-wrap items-start justify-start mx-10 transition duration-75 ease-in-out justify-items-start">
+                @foreach ($data_series as $item)
+                    <div class="m-2 my-2 rounded shadow-lg md:max-w-sm sm:max-w-lg">
+                        <img class="w-full" src="{{ asset('storage/images/course/thumbnail/' . $item->image) }}" alt="Sunset in the mountains">
+                        <div class="px-6 py-4">
+                        <div class="mb-2 text-xl font-bold">
+                            <a href="course/series/{{ Str::slug(strtolower($item->title)) }}">{{ $item->title }}</a>
+                        </div>
+                            <a href="" class="py-1 mr-2 text-blue-500">laravel</a>
+                            <a href="" class="py-1 mr-2 text-blue-500 ">livewire</a>
+                            <a href="" class="py-1 mr-2 text-blue-500">tailwind</a>
+                            <a href="" class="py-1 mr-2 text-blue-500">alpinejs</a>
+                        <p class="mt-5 text-base text-grey-darker">
+                            {{ Str::substr($item->description, 0, 150) }} ...
+                        </p>
+                        </div>
+                        <div class="flex justify-between px-6 py-4 text-xs text-gray-500">
+                            <div>
+                                <span class="font-bold">{{ $item->author->name }}</span>
+                            </div>
+                            <div>
+                                <span class="font-bold">{{ $item->episode->count() }} Lectures</span>
+                                <span class="font-bold">-</span>
+                                @php
+                                    $countSection = DB::table('sections')->where('series_id', $item->id)->distinct()->count();
+                                @endphp
+                                <span class="font-bold">{{ $countSection }} Section</span>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <span class="font-bold">5 Lectures</span>
-                        <span class="font-bold">-</span>
-                        <span class="font-bold">2 Section</span>
-                    </div>
-                </div>
+                @endforeach
             </div>
-
         </div>
-    </div>
-
 
 </div>
